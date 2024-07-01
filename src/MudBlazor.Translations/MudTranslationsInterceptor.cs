0ec0d74484bb109516b3ec038f66f30e89560d6f
Samuel Meenzen <samuel@meenzen.net>
2024-07-01 22:53:37 +0200

    fix: ignore empty translations

--- src/MudBlazor.Translations/MudTranslationsInterceptor.cs
+++ src/MudBlazor.Translations/MudTranslationsInterceptor.cs
@@ -4,14 +4,14 @@ using Microsoft.Extensions.Localization;
 namespace MudBlazor.Translations;
 
 /// <summary>
-/// Provides community translations for the MudBlazor component library.
+/// Provides crowdsourced translations for the MudBlazor component library.
 /// </summary>
 public class MudTranslationsInterceptor : ILocalizationInterceptor
 {
     public LocalizedString Handle(string key, params object[] arguments)
     {
         // rewrite upstream key to match overrides
-        key = key switch
+        string fixedKey = key switch
         {
             "MudDataGrid.=" => "MudDataGrid.Equal",
             "MudDataGrid.!=" => "MudDataGrid.NotEqual",
@@ -22,13 +22,28 @@ public class MudTranslationsInterceptor : ILocalizationInterceptor
             _ => key
         };
 
-        string? translation = LanguageResource.ResourceManager.GetString(key, CultureInfo.CurrentCulture);
+        bool notFound = false;
+        string? translation = LanguageResource.ResourceManager.GetString(fixedKey, CultureInfo.CurrentCulture);
+
+        // Weblate likes to create empty stubs for missing translations, so we need to ignore those and
+        // use english as a fallback.
+        if (!Equals(CultureInfo.CurrentCulture, CultureInfo.InvariantCulture) && string.IsNullOrWhiteSpace(translation))
+        {
+            translation = LanguageResource.ResourceManager.GetString(fixedKey, CultureInfo.InvariantCulture);
+            notFound = true;
+        }
 
         if (translation is not null && arguments.Length > 0)
         {
             translation = string.Format(translation, arguments);
         }
 
-        return new LocalizedString(key, translation ?? key, translation == null);
+        if (translation is null)
+        {
+            translation = key;
+            notFound = true;
+        }
+
+        return new LocalizedString(key, translation, notFound);
     }
 }
