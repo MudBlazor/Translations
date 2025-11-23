0a9886449de3f2c90c8213a8e38b6f33012c9f01
Samuel Meenzen <samuel@meenzen.net>
2025-11-23 16:08:19 +0000

    chore: undo test changes

--- test/MudBlazor.Translations.Tests/TranslationsInterceptorTests.cs
+++ test/MudBlazor.Translations.Tests/TranslationsInterceptorTests.cs
@@ -1,6 +1,4 @@
-using System.Collections;
 using System.Globalization;
-using System.Resources;
 using Microsoft.Extensions.Localization;
 
 namespace MudBlazor.Translations.Tests;
@@ -43,60 +41,5 @@ public class TranslationsInterceptorTests
         result.Value.Should().NotBeNullOrWhiteSpace().And.NotBe(key);
         result.Value.Should().Be(defaultTranslation);
         result.ResourceNotFound.Should().BeFalse();
-    }
-
-    [Fact, UseCulture(culture: "en-US", uiCulture: "nn-NO")]
-    public void All_keys_except_known_identical_keys_are_translated_nnNO() => AssertAllKeys();
-
-    [Fact, UseCulture(culture: "en-US", uiCulture: "nb-NO")]
-    public void All_keys_except_known_identical_keys_are_translated_nbNO() => AssertAllKeys();
-
-    /* Known identical keys between nn-NO/nb-NO and en-US culture */
-    private static readonly HashSet<string> IdenticalAcrossFiles = new(StringComparer.Ordinal)
-    {
-        "MudDataGrid_EqualSign",
-        "MudDataGrid_Filter",
-        "MudDataGrid_GreaterThanOrEqualSign",
-        "MudDataGrid_GreaterThanSign",
-        "MudDataGrid_LessThanOrEqualSign",
-        "MudDataGrid_LessThanSign",
-        "MudDataGrid_NotEqualSign",
-        "MudDataGrid_Operator",
-    };
-
-    private static void AssertAllKeys()
-    {
-        MudTranslationsInterceptor interceptor = new MudTranslationsInterceptor();
-        ResourceManager rm = LanguageResource.ResourceManager;
-
-        ResourceSet? invariantSet = rm.GetResourceSet(
-            CultureInfo.InvariantCulture,
-            createIfNotExists: true,
-            tryParents: true
-        );
-        invariantSet.Should().NotBeNull("we need the neutral resources to compare against");
-
-        foreach (DictionaryEntry entry in invariantSet!)
-        {
-            if (entry.Key is not string key || entry.Value is not string invariantValue)
-            {
-                continue;
-            }
-
-            if (IdenticalAcrossFiles.Contains(key))
-            {
-                continue;
-            }
-
-            // Act
-            LocalizedString result = interceptor.Handle(key);
-
-            // Assert
-            invariantValue.Should().NotBeNullOrWhiteSpace();
-            result.Name.Should().Be(key);
-            result.Value.Should().NotBeNullOrWhiteSpace().And.NotBe(key);
-            result.Value.Should().NotBe(invariantValue);
-            result.ResourceNotFound.Should().BeFalse();
-        }
     }
 }
